5356d2e6929882d402aa9aefc0159ef77ed03761
Jennifer Sandoval <jennifersandoval94@gmail.com>
2019-02-02 14:36:44 -0800

    Updates/cleanup`

--- background.js
+++ background.js
@@ -1,10 +1,9 @@
-console.log('bg running bruh');
-var audio = new Audio('https://ksdt.ucsd.edu/stream.mp3');
+var audio = new Audio('https://d1qg6pckcqcdk0.cloudfront.net/jazz/catsvschicks_fortyyearsofwom_12_anythingyoucando.m4a');
 
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message){
-  if(message === true){
+  if(message == true){
     audio.play();
   } else {
     audio.pause();
